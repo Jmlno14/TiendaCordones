5313757afc5454ac2af49d95c6ffb9dd52481650
Jmlno14 <manololeyva2004@gmail.com>
2023-05-15 20:04:58 -0500

    Repositorio1

---
+++ repositorio2-copy-copy/TiendaCordones.java
@@ -0,0 +1,78 @@
+
+/**
+ * Write a description of class TiendaCordones here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class TiendaCordones {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        Tienda tienda = new Tienda();
+
+        do {
+            System.out.println("=== Tienda de Cordones ===");
+            System.out.println("1. Ver catálogo de cordones");
+            System.out.println("2. Comprar cordones");
+            System.out.println("3. Salir");
+            System.out.print("Ingrese una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    tienda.verCatalogoCordones();
+                    break;
+                case 2:
+                    tienda.comprarCordones();
+                    break;
+                case 3:
+                    System.out.println("Gracias por visitar la tienda. ¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, ingrese una opción válida.");
+                    break;
+            }
+        } while (opcion != 3);
+    }
+}
+
+class Tienda {
+    public void verCatalogoCordones() {
+        System.out.println("=== Catálogo de Cordones ===");
+        System.out.println("1. Cordones Negros");
+        System.out.println("2. Cordones Blancos");
+        System.out.println("3. Cordones Rojos");
+    }
+
+    public void comprarCordones() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("=== Comprar Cordones ===");
+        System.out.println("1. Cordones Negros - $10");
+        System.out.println("2. Cordones Blancos - $12");
+        System.out.println("3. Cordones Rojos - $15");
+        System.out.print("Ingrese el número de cordones que desea comprar: ");
+        opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Ha comprado Cordones Negros por $10. ¡Gracias por su compra!");
+                break;
+            case 2:
+                System.out.println("Ha comprado Cordones Blancos por $12. ¡Gracias por su compra!");
+                break;
+            case 3:
+                System.out.println("Ha comprado Cordones Rojos por $15. ¡Gracias por su compra!");
+                break;
+            default:
+                System.out.println("Opción inválida. Por favor, ingrese una opción válida.");
+                break;
+        }
+    }
+}
+
